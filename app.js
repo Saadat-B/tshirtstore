e089e280e715cd1ec1f3aa8ce5d3e36c1a433627
Saadat Badgujar <saadatbadgujar@gmail.com>
2022-12-24 08:45:29 +0530

    Added express router and created dummy routes

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 require("dotenv").config();
-
 const app = express();
+
+// import all routes here
+
+const home = require("./routes/home");
+
+// router middleware
+app.use("/api/v1", home);
 
 // export app js
 
