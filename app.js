ccadb4fd12ea85e2b3a6b767fe205993a989aebc
Saadat Badgujar <saadatbadgujar@gmail.com>
2022-12-24 09:27:52 +0530

    Added middleware configuration - morgan, cookieParser, fileUpload, swaggerDocs, Yamljs, express.json, express.urlencoded

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+
+// for swagger documentation
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// morgan middleware
+app.use(morgan("tiny"));
+
+//regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookies and file middleware
+app.use(cookieParser());
+app.use(fileUpload());
 
 // import all routes here
 
