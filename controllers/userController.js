cfa12a731b0ef8a6fe2da7498be601ceffa2f686
Saadat Badgujar <saadatbadgujar@gmail.com>
2023-01-04 20:43:05 +0530

    Added user details update route and controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -188,3 +188,39 @@ exports.changePassword = BigPromise(async (req, res, next) => {
 
   cookieToken(user, res);
 });
+
+exports.updateUserDetails = BigPromise(async (req, res, next) => {
+  const newData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  if (req.files) {
+    const user = await User.findById(req.user.id);
+    const imageId = user.photo.id;
+
+    const resp = await cloudinary.v2.uploader.destroy(imageId);
+
+    let file = req.files.photo;
+    result = await cloudinary.v2.uploader.upload(file.tempFilePath, {
+      folder: "users",
+      width: 150,
+      crop: "scale",
+    });
+
+    newData.photo = {
+      id: result.public_id,
+      secure_url: result.secure_url,
+    };
+  }
+
+  const user = await User.findByIdAndUpdate(req.user.id, newData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
