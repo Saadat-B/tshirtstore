724d240e5afea73672b2118f976c36c543d62abf
Saadat Badgujar <saadatbadgujar@gmail.com>
2023-01-05 14:16:24 +0530

    Added add product controller and removed test product controller and route

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const { testProduct } = require("../controllers/productController");
+const {} = require("../controllers/productController");
 const router = express.Router();
 
 const { isLoggedIn, customRole } = require("../middlewares/user");
-
-router.route("/testproduct").get(testProduct);
 
 module.exports = router;
